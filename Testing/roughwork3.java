6e9653aa3ad838312c8fdd9b6ae22917f086bc0f
Raviprasaath <raviprasaath1@gmail.com>
2023-05-03 20:37:08 +0530

    05-03-2023

--- Testing/roughwork3.java
+++ Testing/roughwork3.java
@@ -4,20 +4,6 @@ import java.util.Stack;
 
 public class roughwork3 {
     public static void main(String[] args) {
-        int n = 13;
-        System.out.println(countDigitOne(n));
-    }
-    public static int countDigitOne(int n) {
-        String num =Integer.toBinaryString(n);
-        int ans = 0;
-        System.out.println(num);
-        for (int i=0; i<num.length(); i++) {
-            if (num.charAt(i) == '1') {
-                ans++;
-            }
-        }
-        return ans;
-    }
-    
 
+    }
 }
