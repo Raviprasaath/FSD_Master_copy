8eb82ef9d138181abd5dee140e721340dde3361e
Raviprasaath <raviprasaath1@gmail.com>
2023-05-15 08:05:05 +0530

    05-15-2023

--- Testing/roughwork3.java
+++ Testing/roughwork3.java
@@ -1,7 +1,20 @@
 package Testing;
 
+import java.util.ArrayList;
+
 public class roughwork3 {
     public static void main(String[] args) {
         
+        int [] array1 = {1,2,3,4,5};
+        int [] array2 = {3,4,5,1,2};
+
+        ArrayList <Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < array2.length; i++) {
+            list.add(array2[i]);
+        }
+    
+    
     }
+
 }
