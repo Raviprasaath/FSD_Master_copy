77e619044902a2d613eb4ba0c686671ad5a10b53
Raviprasaath <raviprasaath1@gmail.com>
2023-05-28 07:39:59 +0530

    05-28-2023

--- FrontEnd/JS/JS_LiveClass18_AsyncAwait/script.js
+++ FrontEnd/JS/JS_LiveClass18_AsyncAwait/script.js
@@ -45,31 +45,31 @@ fetchQuote();
 // Response {type: 'cors', url: 'https://api.kanye.rest/', redirected: false, status: 200, ok: true, …}
 */
 
-async function fetchQuotes() {
+// async function fetchQuotes() {
     
-    const response = await fetch("https://api.kanye.rest/")
+//     const response = await fetch("https://api.kanye.rest/")
     
-    const data = await response.json();
-    console.log(data);
+//     const data = await response.json();
+//     console.log(data);
    
-    const {quote} = data //---> const quotes = data.quotes;
-    console.log(quote)
+//     const {quote} = data //---> const quotes = data.quotes;
+//     console.log(quote)
     
-    addTotheDom(quote)
-}
+//     addTotheDom(quote)
+// }
 
 
 // output -> (random quotes were generating)
 
 
-function addTotheDom(quote) {
-    const h1Elem = document.createElement('h1');
-    h1Elem.innerHTML = quote;
+// function addTotheDom(quote) {
+//     const h1Elem = document.createElement('h1');
+//     h1Elem.innerHTML = quote;
     
-    const body = document.body;
-    body.appendChild(h1Elem);
-}
-fetchQuotes()
+//     const body = document.body;
+//     body.appendChild(h1Elem);
+// }
+// fetchQuotes()
 
 // In web page the quote were updated in H1 tag element
 
@@ -80,4 +80,69 @@ fetchQuotes()
 // resolve means to get the real data
 // the promise(fetch) created by Java script prople
 // server giving the data's to fetch as response object
-//  
+// response object had more huge things, we need to change into playing text or json
+// json is simplest form of object
+// await used to here because json conversion take some time 
+// .json also promise, we need to wait for that so await using
+ 
+
+// console.log('hello')
+// fetch("https://api.kanye.rest/") 
+//     .then(response => {
+//         return response.json()
+//     }).then (data => {
+//         console.log('data')
+//     }).catch (error => {
+//         console.log('error')
+//     }) 
+// console.log('bye');
+
+// output
+// hello
+// bye
+// data
+
+// if link is not correct means
+// hello
+// bye
+// error
+
+
+
+
+
+
+
+
+
+
+// Promise changeing  
+
+console.log('hello')
+fetch("https://api.kanye.rest/") 
+    .then(response => {
+        // response handler
+        return response.json()    // here .then block is we changing the promise (or) then block returing a promise
+    }).then (data => {
+        // data handler
+        console.log('data handler 0', data)
+        
+        return '1 output'// -> if i comment it means it give undefined in below data handler 1, if not then give value as 1
+
+    }).then (data => {
+        // number 1 handler
+        console.log('data handler 1', data)        
+    }).then (data => {
+        // number 2 handler
+        
+        console.log('data handler 2', data) //here this give undefined because there is no return value in previous block
+        
+    }).then (data => {
+        // number 3 handler
+    })
+    
+    
+    .catch (error => {
+        console.log('error')
+    }) 
+console.log('bye');
