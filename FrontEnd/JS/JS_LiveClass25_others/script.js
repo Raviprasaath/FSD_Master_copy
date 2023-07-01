e6b12d23cdd08922d50556e15cd693e2c54c664d
Raviprasaath <raviprasaath1@gmail.com>
2023-07-01 07:40:09 +0530

    07-01-2023

--- FrontEnd/JS/JS_LiveClass25_others/script.js
+++ FrontEnd/JS/JS_LiveClass25_others/script.js
@@ -76,3 +76,74 @@ myFetch.then(data=> console.log(data));
 // 1
 // here also promise is prior but hello return in 0 sec, then fetch was return
 
+
+
+
+//----------------------------
+// function outerfunction(x) {
+//     function innerfunction(y) {
+//         return x + y;
+//     }
+//     return innerfunction;
+// }
+
+// let a = 12;
+// let funcf = outerfunction(a);
+// let res = funcf(2);
+// console.log(res);   // ans 14
+
+//----------------------------
+// function outerfunction(x) {
+//     function innerfunction(y) {
+//         return x + y;
+//     }
+//     return innerfunction;
+// }
+
+// let a = 12;
+// let funcf = outerfunction(a);
+// a=99;   // a will be not update
+// let res = funcf(2);
+// console.log(res);   // ans 14
+//-----------------------------
+function outerfunction(ob) {
+    function innerfunction(y) {
+        return ob.x + y;
+    }
+    return innerfunction;
+}
+
+let obj = {x: 12};
+let funcf = outerfunction(obj);
+obj.x=99;   // here x:12 stored in heap, so same address was pointing, thats why value get updated
+let res = funcf(2);
+console.log(res);   // ans 101
+//-----------------------------
+
+var x = 1;
+function outer() {
+    console.log(x);
+
+    var x = 2;
+    function inner() {
+        console.log(x);
+    }
+    inner();
+}
+outer();
+// output ->   undefined 2
+// ------------
+var b = 1;
+function outer() {
+    var b = 2;
+    function inner() {
+        b++;
+        var b = 4;
+        console.log(b);
+    }
+    inner();        
+}
+let x = outer;
+let y = x();
+
+// output = 4
