9d45bdabf2ebf3a467bac3ad6d16755f3788d403
Raviprasaath <raviprasaath1@gmail.com>
2023-07-04 20:19:39 +0530

    07-04-2023

--- Testing/roughwork.java
+++ Testing/roughwork.java
@@ -3,34 +3,23 @@ import java.util.*;
 
 public class roughwork {
     public static void main(String[] args) {
-        int arr [] = {1,1,2,3,4,2,5,5,5,5,2,3,4,5,6,7,7,7,7,7,7};
-
-        HashMap <Integer, Integer> hmap = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            hmap.put(arr[i], hmap.getOrDefault(arr[i], 0)+1);
-        }
-        System.out.println(hmap);
-        
-        int max = 0;
-        for (int i : hmap.values()) {
-            if (i > max) {
-                max = Math.max(i, max);
-            }    
-        } 
-        System.out.println(max);
-
-        for (int i : hmap.keySet()) {
-            if (i > max) {
-                max = Math.max(i, max);
-            }    
-        } 
-        System.out.println(max);
-
-        for (int i : hmap.keySet()) {
-            if (i % 2 == 0) {
-                System.out.print(i+" ");                    
+        String s = "leetscode";
+        String [] dictionary = {"leet","code","leetcode"};
+        System.out.println(minExtraChar(s, dictionary));
+    }   
+    public static int minExtraChar(String s, String[] dictionary) {
+        int k = 0, n = 0;
+        StringBuilder sb  = new StringBuilder(s);
+        while (k != dictionary.length-1) {
+            if (s.contains(dictionary[k])) {
+                n = sb.indexOf(dictionary[k]);
+                sb.delete(n, dictionary[k].length());
             }
+            k++;
         }
-    }        
+        System.out.print(sb.toString());
+        return n;
+        // return sb.length();
+    }     
 }
 
