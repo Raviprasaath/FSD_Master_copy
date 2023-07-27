0d6f75e5254ef5061f0b3aaac14558ea258a2604
Raviprasaath <raviprasaath1@gmail.com>
2023-07-27 07:14:38 +0530

    07-27-2023

---
+++ FrontEnd/React/learning_1/src/components/MyHeader/MyHeader.js
@@ -0,0 +1,12 @@
+import "./MyHeader.css";
+
+function MyHeader() {
+  return (
+    <>
+      <h1> My custom Header</h1>
+      Name: <input type="text"></input>
+    </>
+  );
+}
+
+export default MyHeader;
