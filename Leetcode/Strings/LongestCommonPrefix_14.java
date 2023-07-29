dcd40f42f468793f62af4ad37ba82f23901ccdbd
Raviprasaath <raviprasaath1@gmail.com>
2023-07-29 07:58:42 +0530

    07-29-2023

--- Leetcode/Strings/LongestCommonPrefix_14.java
+++ Leetcode/Strings/LongestCommonPrefix_14.java
@@ -6,7 +6,8 @@ public class LongestCommonPrefix_14 {
         String [] str = {"flower","flow","flight"};
         System.out.println(longestCommonPrefix(str));
     }
-    public static String longestCommonPrefix(String[] strs) {
+    // poor method
+    public static String longestCommonPrefix1(String[] strs) {
         Arrays.sort(strs);
         String s = strs[0];
         String e = strs[strs.length-1];
@@ -17,4 +18,19 @@ public class LongestCommonPrefix_14 {
         }
         return s;
     }
+    
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i=1; i<strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
 }
+
+
