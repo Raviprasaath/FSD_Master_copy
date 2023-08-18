56be5cd8d817c9509e3a63fa17f6a0188b1e5a17
Raviprasaath <raviprasaath1@gmail.com>
2023-08-18 06:50:20 +0530

    08-18-2023

--- React/FlightBookingWebsite/src/App.jsx
+++ React/FlightBookingWebsite/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar.jsx'
+import Home from './Components/Home/Home.jsx'
+import Search from './Components/Search/Search.jsx'
+import Support from './Components/Support/Support.jsx'
 import './App.css'
 
 function App() {
@@ -7,6 +10,9 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
+      <Search/>
+      <Support/>
     </>
   )
 }
---
+++ React/FlightBookingWebsite/src/Components/Support/Support.jsx
@@ -0,0 +1,56 @@
+const Support = () => {
+  return (
+    <div className="suppport container section">
+      <div className="sectionContainer">
+        <div className="titlesDiv">
+          <small>travel support</small>
+          <h2>Plan your travel with confidence</h2>
+          <p>
+            Find help with booking and travel plans, see what to expect along
+            the journey!
+          </p>
+        </div>
+
+        <div className="infoDiv grid">
+
+          <div className="textDiv grid">
+            <div className="singleInfo">
+              <span className="number">01</span>
+              <h4>Travel requirements for Dubai</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey!
+              </p>
+            </div>
+
+            <div className="singleInfo">
+              <span className="number">02</span>
+              <h4>Chauffeur services at your arrival</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey!
+              </p>
+            </div>
+
+            <div className="singleInfo">
+              <span className="number">03</span>
+              <h4>Multi-risk travel insurance</h4>
+              <p>
+                Find help with booking and travel plans, see what to expect
+                along the journey!
+              </p>
+            </div>
+          </div>
+
+          <div className="imgDiv">
+            
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
